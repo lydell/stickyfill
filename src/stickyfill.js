ef2e101b83d45fdfe0fb659ae7c34e048f7637c8
Simon Lydell <simon.lydell@gmail.com>
2017-01-24 14:57:05 +0100

    Guard against `el.clone` already being removed from the DOM

--- src/stickyfill.js
+++ src/stickyfill.js
@@ -222,7 +222,9 @@
     }
 
     function killClone(el) {
-        el.clone.parentNode.removeChild(el.clone);
+        if (el.clone.parentNode) {
+            el.clone.parentNode.removeChild(el.clone);
+        }
         el.clone = undefined;
     }
 
